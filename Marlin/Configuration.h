09018a6a3a14e8a8ef346d698a0e163340ab15c0
DESKTOP-CBCQBF6\vamon <vamone13@hotmail.com>
2018-05-28 12:25:05 +0100

    Configuration without BL-Touch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -67,6 +67,7 @@
  * Z-Probe type (must be none or one of them)
  * If a Z-Probe type is selected, a Bed Leveling type other than MANUAL must be selected.
   */
+ //#define BLTOUCH //
 //#define SN04          // Green sensor
 //#define INDUCTIVE_NO  // Normally open inductive sensor
 //#define INDUCTIVE_NC  // Normally closed inductive sensor
@@ -78,9 +79,9 @@
  */
 //#define TRIPOINT
 //#define LINEAR
-#define BILINEAR
+//#define BILINEAR
 //#define UBL
-//#define MANUAL
+#define MANUAL
 
 /**
  * Z-Probe offset from nozzle (https://github.com/JimBrown/MarlinTarantula/wiki/How-to-determine-your-Z-Probe-offset)
